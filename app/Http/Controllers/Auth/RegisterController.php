4cb1f9edb20d9c9abf1fcc6667733927b55c5fae
Gomin <teelak1113@gmail.com>
2020-11-16 21:32:25 +0700

    fix register and migrate

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'address' => $data['address'],
+            'phone_number' => $data['phone_number'],
             'password' => Hash::make($data['password']),
         ]);
     }
